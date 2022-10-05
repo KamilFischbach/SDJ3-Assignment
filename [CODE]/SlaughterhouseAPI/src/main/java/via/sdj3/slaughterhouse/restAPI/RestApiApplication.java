c1cd237f7b3397a57372d06f0ef1364df6c56985
KamilFischbach <92295061+KamilFischbach@users.noreply.github.com>
2022-10-05 12:20:57 +0200

    Setting up the RESTAPI
    I dont know how to start the tomcat server using the mvn commands

---
+++ [CODE]/SlaughterhouseAPI/src/main/java/via/sdj3/slaughterhouse/restAPI/RestApiApplication.java
@@ -0,0 +1,13 @@
+package via.sdj3.slaughterhouse.restAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+}
---
+++ [CODE]/SlaughterhouseAPI/src/test/java/via/sdj3/slaughterhouse/restAPI/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package via.sdj3.slaughterhouse.restAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
